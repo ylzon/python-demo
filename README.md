cfa98d9ae7ecd5ab32b17f5a8b7a767501e97161
mgss <admin@ylzzxt.cn>
2017-09-25 17:02:40 +0800

    fix demo bugs & fix README

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Python-demo
-新手用来练手的Python小Demo合集
+个人用来练手的Python小Demo合集
 
 ## 目录
 
@@ -10,16 +10,15 @@
 * 持续更新中...
 
 ## 运行Demo
-* 推荐先根据目录中的题目先看一遍需求，自己直接动手做一遍，再运行我的Demo做对比
 * 下载源码 `git clone git@github.com:mgss/python-demo.git`
 * 进入相应目录执行 `python demo1.py`
-* 所有Demo 基于 Python 2.7.11
+* 所有Demo 基于 Python 3.6
 * 推荐在Linux或macOS环境下执行
 
 ## 关于项目
 
-* 本项目提供作者学习Python一路以来练习过的Demo，并在持续发布中...
+* 本项目是个人学习Python一路以来练习过的Demo，并在持续更新中...
 * 觉得还可以的朋友欢迎star，认为有更好的练习Demo欢迎[issues](https://github.com/mgss/python-demo/issues)
-* 作者博客：[ylzzxt.cn](https://ylzzxt.cn/)
+* 个人博客：[ylzzxt.cn](https://ylzzxt.cn/)
 
 
--- example/basic/demo4.py
+++ example/basic/demo4.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
 # 作业四：
@@ -7,7 +6,8 @@
 # 显示商品列表，让用户根据序号选择商品，加入购物车
 # 购买，如果商品总额大于总资产，提示账户余额不足，否则，购买成功。
 # 附加：可充值、某商品移除购物车
-import os
+# import os
+
 
 goods = [
     {"name": "电脑", "price": 1999},
@@ -15,73 +15,99 @@ goods = [
     {"name": "游艇", "price": 20},
     {"name": "美女", "price": 998},
 ]
-os.system("clear")
-total = int(input("请输入您的总资产："))
+
+while True:
+
+    total_inp = input("请输入您的总资产：")
+    if total_inp == "":
+
+        print("指令输入有误，请重新选择！")
+        continue
+    else:
+        total = int(total_inp)
+        break
+
 cart = []  # 购物车
 cart_sum = 0  # 总价格
 cart_size = 0  # 总数量
 while True:
 
-    print "=======商品栏======="
+    print("=======商品栏=======")
     for i in range(0, len(goods)):
-        print i + 1, goods[i].get('name')
-    print "0 结束购物，查看购物车"
-    print "==================="
-    opt = int(input("请选择："))
+        temp = goods[i].get('name')
+        print(i+1,temp)
+    print("0 结束购物，查看购物车")
+    print("===================")
+    opt_inp = input("请选择：")
+    if opt_inp == "":
 
+        print("指令输入有误，请重新选择！")
+        continue
+    else:
+        opt = int(opt_inp)
     if opt == 0:
         while True:
-            os.system("clear")
-            print "=======购物车======="
-            print "总资产：", total
-            print "总价格：", cart_sum
-            print "总数量：", cart_size
-            for i in range(0, len(cart)):
-                print i + 1, cart[i].get('name'), "价格", cart[i].get('price')
-            print "9  [充值]"
-            print "0  [结算]"
-            print "删除商品请输入商品对应序号"
-            print "==================="
 
-            opt2 = int(input("请选择："))
+            print("=======购物车=======")
+            print("总资产：", total)
+            print("总价格：", cart_sum)
+            print("总数量：", cart_size)
+            for i in range(0, len(cart)):
+                print(i + 1, cart[i].get('name'), "价格", cart[i].get('price'))
+            print("8  [返回]")
+            print("9  [充值]")
+            print("0  [结算]")
+            print("删除商品请输入商品对应序号")
+            print("===================")
+
+            opt2_inp = input("请选择：")
+            if opt2_inp == "":
+
+                print("指令输入有误，请重新选择！")
+                continue
+            else:
+                opt2 = int(opt2_inp)
+
+            # 返回项
+            if opt2 == 8:
+                break
 
             # 充值项
-            if opt2 == 9:
-                os.system("clear")
+            elif opt2 == 9:
                 top_up = int(input("请输入要充值的金额数："))
                 total += top_up
-                print "充值成功"
+                print("充值成功")
                 continue
 
             # 结算项
             elif opt2 == 0:
-                # !bug! macOS环境下，iTerm2终端下，Python2.7.11下该操作无效，PyCharm环境的命令行执行成功，该BUG尚未解决 2017年09月24日23:13:05 !bug!
                 if cart_sum > total:
-                    os.system("clear")
-                    print "您的资产余额不足，还差", cart_sum - total, "请充值后重新结算"
+
+                    print("您的资产余额不足，还差", cart_sum - total, "请充值后重新结算")
                     continue
                 else:
                     if cart_sum == 0:
-                        os.system("clear")
-                        print ("您尚未选购商品，请先选择商品并加入购物车！")
+                        #
+                        print("您尚未选购商品，请先选择商品并加入购物车！")
                         break
                     else:
-                        os.system("clear")
-                        print "已支付", cart_sum, "元，购买成功！"
+                        #
+                        print("已支付", cart_sum, "元，购买成功！")
                         total -= cart_sum
                         cart = []
                         cart_size = 0
                         cart_sum = 0
                         continue
+
             # 删除商品项
             else:
                 if opt2 > cart_size:
-                    os.system("clear")
-                    print "指令输入有误，请重新选择！"
+                    # 
+                    print("指令输入有误，请重新选择！")
                     continue
                 else:
-                    os.system("clear")
-                    print "已删除价格为[", cart[opt2 - 1].get('price'), "]的商品[", cart[opt2 - 1].get('name'), "]"
+
+                    print("已删除价格为[", cart[opt2 - 1].get('price'), "]的商品[", cart[opt2 - 1].get('name'), "]")
                     cart_size -= 1
                     cart_sum -= cart[opt2 - 1].get('price')
                     del cart[opt2 - 1]
@@ -89,13 +115,13 @@ while True:
 
     # 添加商品到购物车
     else:
-        if opt > len(goods):
-            os.system("clear")
-            print "指令输入有误，请重新选择！"
+        if opt > len(goods) or opt == "":
+
+            print("指令输入有误，请重新选择！")
             continue
         else:
-            os.system("clear")
-            print "已添加价格为[", goods[opt - 1].get('price'), "]的商品[", goods[opt - 1].get('name'), "]到购物车"
+
+            print("已添加价格为[", goods[opt - 1].get('price'), "]的商品[", goods[opt - 1].get('name'), "]到购物车")
             cart.append(goods[opt - 1])
             cart_size += 1
             cart_sum += goods[opt - 1].get('price')
