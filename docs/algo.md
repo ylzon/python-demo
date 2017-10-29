452c4ac682755119a333eff27ae843a09e213cb2
mgss <admin@ylzzxt.cn>
2017-10-29 13:01:17 +0800

    fix algo demo1

--- docs/algo.md
+++ docs/algo.md
@@ -2,6 +2,12 @@
 
 ## Demo-1
 
-*
+* 冒泡排序
 
  参考Demo：[链接](https://github.com/mgss/python-demo/blob/master/example/algo/demo1.py)
+
+## Demo-2
+
+* 递归
+
+ 参考Demo：[链接](https://github.com/mgss/python-demo/blob/master/example/algo/demo2.py)
--- example/algo/demo1.py
+++ example/algo/demo1.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
+
+li = [11, 1, 1, 1, 1,  2, 35, 7, 34, 8, 1, 123, 23, 34, 511, 213, 34, 56, 0]
+
+
+# 冒泡排序
+def bubbling(arr):
+    new_list = list(arr)
+
+    for i in range(1, len(new_list)):
+        for j in range(len(new_list) - i):
+
+            if new_list[j] > new_list[j + 1]:
+                temp = new_list[j]
+                new_list[j] = new_list[j + 1]
+                new_list[j + 1] = temp
+
+    return new_list
+
+
+new = bubbling(li)
+print(new)
