f939e1e9947e24bae4e0185c11eb1be73277825b
EvanBell <admin@ylzzxt.cn>
2017-10-29 22:24:49 +0800

    fix base demo16

--- example/basic/demo16.py
+++ example/basic/demo16.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-# 装饰器
+
+
+# 多层装饰器
+def outer_fist(func):
+    def inner(*args, **kwargs):
+        print("第一层开始")
+        ret = func(*args, **kwargs)
+        print("第一层结束")
+        return ret
+
+    return inner
+
 
 def outer(func):
     def inner(*args, **kwargs):
@@ -13,6 +24,7 @@ def outer(func):
     return inner
 
 
+@outer_fist
 @outer
 def main(a1, a2):
     print("内容")
