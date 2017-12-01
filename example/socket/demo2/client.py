0d59dfb922c9945ab685ae1f4a21935453fa35a0
mgss <admin@ylzzxt.cn>
2017-12-01 19:48:42 +0800

    添加开始上传和结束上传

--- example/socket/demo2/client.py
+++ example/socket/demo2/client.py
@@ -27,10 +27,13 @@ sk.sendall(bytes(str(file_size), encoding="utf-8"))
 # 接受服务端收到文件大小，防止粘包
 sk.recv(1024)
 
+print("开始上传")
 # 发送文件
 with open(file_path, "rb") as f:
     for line in f:
         sk.sendall(line)
+print("上传结束")
+
 
 # 关闭连接
 sk.close()
